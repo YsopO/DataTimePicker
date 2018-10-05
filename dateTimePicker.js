747b95abf975b07b0a7ac82d311cbb85ad513693
YsopO <brommundt@posteo.de>
2018-10-05 14:28:19 +0200

    vorButtonZugriff

--- dateTimePicker.js
+++ dateTimePicker.js
@@ -18,10 +18,48 @@ function create(htmlElement, parent, className, id) {
 }
 function dateTimeTable(parent) {
     var date = new Date(Date.now());
-    var weekday = date.getDate();
-    console.log(weekday);
+    var months = ["Januar", "Februar", "März", "April", "Mai", "Juni", "Juli", "August", "September", "Oktober", "November", "Dezember"];
+    var lengthOfMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+    var indexMonth = date.getMonth();
+    var year = date.getFullYear();
+    date = new Date(date.getFullYear() + "-" + (date.getMonth() + 1) + "-01");
+    var weekday = date.getDay();
     var myTable = create("table", parent, "", "");
     var myTableHead = create("thead", myTable, "", "");
+    var myHeadRow1 = create("tr", myTableHead, "", "");
+    var thleft = create("th", myHeadRow1, "", "");
+    var left = create("button", thleft, "", "left");
+    left.innerText = "<<";
+    var colspanTh5 = create("th", myHeadRow1, "", "");
+    colspanTh5.colSpan = 5;
+    var month = create("label", colspanTh5, "", "month");
+    left.addEventListener("click", function (e) {
+        if (indexMonth == 0) {
+            indexMonth = 11;
+            year--;
+            month.innerText = months[indexMonth] + " " + year;
+        }
+        else {
+            indexMonth--;
+            month.innerText = months[indexMonth] + " " + year;
+        }
+    });
+    month.innerText = months[indexMonth] + " " + year;
+    var thright = create("th", myHeadRow1, "", "");
+    var right = create("button", thright, "", "right");
+    right.innerText = ">>";
+    right.style.cssFloat = "right";
+    right.addEventListener("click", function (e) {
+        if (indexMonth == 11) {
+            indexMonth = 0;
+            year++;
+            month.innerText = months[indexMonth] + " " + year;
+        }
+        else {
+            indexMonth++;
+            month.innerText = months[indexMonth] + " " + year;
+        }
+    });
     var myHeadRow = create("tr", myTableHead, "", "");
     var days = ["Mo", "Di", "Mi", "Do", "Fr", "Sa", "So"];
     for (var x = 0; x < 7; x++) {
@@ -39,6 +77,8 @@ function dateTimeTable(parent) {
         });
         return row;
     });
+    var test = Array.prototype.slice.call(document.querySelector("tbody").querySelectorAll("button"));
+    console.log(test);
 }
 dateTimeTable(document.getElementById("datatimepicker"));
 console.log("ende");
--- dateTimePicker.ts
+++ dateTimePicker.ts
@@ -16,12 +16,56 @@ function create<T>(htmlElement:string, parent:HTMLElement,className:string,id:st
 }
 
 function dateTimeTable(parent:HTMLElement){
+    
     let date = new Date(Date.now())
-    let weekday = date.getDate()
-    console.log(weekday)
-    let left = create("")
+    let months = ["Januar","Februar","März","April","Mai", "Juni","Juli", "August", "September", "Oktober","November","Dezember"]
+    let lengthOfMonth = [31,28,31,30,31,30,31,31,30,31,30,31]
+    let indexMonth = date.getMonth()
+    let year = date.getFullYear()
+    date = new Date(`${date.getFullYear()}-${date.getMonth()+1}-01`)
+    let weekday = date.getDay()
     let myTable = create("table",parent,"","")
     let myTableHead = create("thead",myTable,"","")
+    let myHeadRow1 = create("tr",myTableHead,"","")
+    let thleft = create("th",myHeadRow1,"","")
+    let left = create("button", thleft,"","left")
+    left.innerText = "<<"
+
+    let colspanTh5 = create("th",myHeadRow1,"","") as HTMLTableHeaderCellElement
+    colspanTh5.colSpan = 5
+
+    let month = create("label",colspanTh5,"","month")
+    
+    left.addEventListener("click",e=>{
+        if (indexMonth==0) {
+            indexMonth = 11
+            year--
+            month.innerText = `${months[indexMonth]} ${year}`
+        }
+        else {
+            indexMonth--
+            month.innerText = `${months[indexMonth]} ${year}`
+        }
+    })
+
+
+    month.innerText = `${months[indexMonth]} ${year}`
+    let thright = create("th",myHeadRow1,"","")
+    let right = create("button", thright,"","right")
+    right.innerText = ">>"
+    right.style.cssFloat = "right"
+    right.addEventListener("click",e=>{
+        if (indexMonth==11) {
+            indexMonth = 0
+            year++
+            month.innerText = `${months[indexMonth]} ${year}`
+        }
+        else {
+            indexMonth++
+            month.innerText = `${months[indexMonth]} ${year}`
+        }
+    })
+
     let myHeadRow = create("tr",myTableHead,"","")
     let days=["Mo","Di","Mi","Do","Fr","Sa","So"]
     for (let x=0;x<7;x++){
@@ -39,6 +83,8 @@ function dateTimeTable(parent:HTMLElement){
         })
         return row
     })
+    let test = Array.prototype.slice.call((document.querySelector("tbody") as HTMLElement).querySelectorAll("button"))
+    console.log(test)
 }
 
 dateTimeTable(document.getElementById("datatimepicker") as HTMLElement)
